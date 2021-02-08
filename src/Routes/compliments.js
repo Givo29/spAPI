5d8868f01cdb096f4817dc32c270edb828a7077f
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2021-02-08 11:05:48 +1100

    Add compliment model, routes for compliments

---
+++ src/Routes/compliments.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Compliment = require('../Models/compliment');
+
+router.get('/', async (_, res) => {
+    try {
+        let count = await Compliment.count();
+        let random = Math.floor(Math.random() * count);
+        const compliment = await Compliment.findOne().skip(random);
+        res.json(compliment)
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const compliment = new Compliment({
+        string: req.body.string
+    });
+
+    try {
+        const newCompliment = await compliment.save();
+        res.status(201).json(newCompliment);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const compliment = require('./Models/compliment');
+
+// Routes
+const complimentRoute = require('./Routes/compliments');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/compliment', complimentRoute);
 
-app.get("/api/compliment", (req, res) => {
-    Compliment.findOne({}, (err, compliment) => {
-        if (err) return console.log(err);
-        res.send(compliment);
-    })
+mongoose.connect('mongodb://api-database:27017/api', {
+    'auth': { 'authSource': 'admin' },
+    'user': 'root',
+    'pass': 'password',
+    'useMongoClient': true
 });
 
-app.post("/api/compliment", (req, res) => {
-    const comp = new Compliment({
-        string: req.body.string,
-    });
-    comp.save()
-    .then(val => {
-        res.json({ msg: "Compliment added", val: val })
-    });
-});
-
-mongoose.connect('mongodb://root:password@api-database:27017', { useNewUrlParser: true });
-
-mongoose.connection.once('open',_ => {
-    console.log('Database connected Successfully');
-})
-.on('error', err => {
+mongoose.connection.on('error', err => {
     console.log('Error', err);
 });
-
-// MongoClient.connect('mongodb://root:password@api-database:27017', { useUnifiedTopology: true })
-// .then(client => {
-//     console.log('Connected to database');
-//     const db = client.db('api');
-// });
-
-app.get("/", (req, res) => {
-    res.send("Hello There");
-});
-
-app.get("/hello", (req, res) => {
-    res.send("There");
+mongoose.connection.once('open', _ => {
+    console.log('Database connected Successfully');
 });
 
 app.listen(3000, _ => {
